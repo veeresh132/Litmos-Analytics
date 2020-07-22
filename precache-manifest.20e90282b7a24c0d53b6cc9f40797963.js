2b7c74cb1d2b76b72a83debee94cd5e13b2d9ecb
Neralagi <veeresh.neralagi@sap.com>
2020-07-22 11:27:45 -0700

    Updates

---
+++ precache-manifest.20e90282b7a24c0d53b6cc9f40797963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3900e5cbc72672afdbaeb5ffb98e9aa4",
+    "url": "/Litmos-Analytics/index.html"
+  },
+  {
+    "revision": "89c72f6bcdadad39494d",
+    "url": "/Litmos-Analytics/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3867f4b6960120c8b99",
+    "url": "/Litmos-Analytics/static/js/2.5c249f36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Litmos-Analytics/static/js/2.5c249f36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c72f6bcdadad39494d",
+    "url": "/Litmos-Analytics/static/js/main.b7064c3f.chunk.js"
+  },
+  {
+    "revision": "4b81e9a4f936b06ef219",
+    "url": "/Litmos-Analytics/static/js/runtime-main.6dc644c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Litmos-Analytics/static/media/logo.5d5d9eef.svg"
+  }
+]);
